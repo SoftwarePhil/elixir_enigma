b4f4302b24c8337168a911fad455390e364ec1d3
Phil DiMarco <plantlife26@gmail.com>
2016-08-17 14:52:23 -0400

    added some rotors ~~

--- lib/enigma/wheel.ex
+++ lib/enigma/wheel.ex
@@ -1,4 +1,26 @@
-#I don't know if I need a struct ..
 defmodule Enigma.Wheel do
-defstruct []
+@abc 'abcdefghigklmnopqrstuvwxyz'
+@IC {3, 11, 17, 19, 14, 3, 5, 10, 12, 18, 6, 2, -2, -8, -10, -6, -14, -17, 7, -18, -5, -15, 1, -9, -17, -4}
+@IIC {7, 15, 23, 3, 11, 4, 13, 5, 6, -5, 1, 2, -10, -5, -9, -12, 8, -17, 4, 2, -16, -1, -4, -6, -14, -2}
+@IIIC {20, 15, 11, 16, 7, 13, 19, -2, 4, 11, -6, -4, -9, 2, 9, -5, -8, -16, 3, 5, -14, -12, -20, -1, -10, -25}
+
+#these are how I generated the above rotors ...
+def create_wheel(rotor) do
+  _create_wheel('', rotor, @abc)
+end
+
+defp _create_wheel(old_wheel, [head|sequence], [head2| abc]) do
+  old_wheel ++ [find_shift(head, head2)]
+  |>_create_wheel(sequence, abc)
+end
+
+defp _create_wheel(wheel, [], []) do
+  wheel
+  |>List.to_tuple
+end
+
+defp find_shift(code_letter, abc_letter) do
+  code_letter - abc_letter
+end
+
 end
